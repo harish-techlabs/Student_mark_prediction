31ee6b74ba1188bfc3913c0db995d5e0df396570
Harish-innovator <harishsreni@gmail.com>
2026-04-26 19:52:12 +0530

    deployment

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ import streamlit as st                           # importing the necessary packa
 import joblib
 import numpy as np
 
-model = joblib.load()
+model = joblib.load("linear_model.pkl")                       # loading the model
 st.title("student marks prediction")
 
-courses=st.number_input("no of courses you choosen to study____Enter here:")
-hours=st.number_input("how many hours take to study____Enter here:")
+st.write("Enter the following details to predict the marks of the student")
 
+number_of_courses = st.number_input("Enter the number of courses are enrolled in:",min_value=0,max_value=100,step=1)       # taking input from the user
+study_hours = st.number_input("Enter the number of hours to study : ",min_value=0,max_value=100,step=1) 
 
- 
+if st.button("Predict"):
+   input_data = np.array([[study_hours, number_of_courses]])
+   prediction = model.predict(input_data)
+   st.write(f"The predicted marks of the student is: {prediction[0]}")
